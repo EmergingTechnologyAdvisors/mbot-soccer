caba8d3d1b090fe7b8cdc7eeaae60870e2ba42bc
Linda Nichols <lynnaloo@gmail.com>
2016-03-27 23:15:46 -0400

    Switched controls back to match m1 with the left motor

--- eg/controls.js
+++ eg/controls.js
@@ -18,8 +18,8 @@ board.on('ready', function (err) {
   }
 
   const motors = {
-    right: new five.Motor([6, 7]),
-    left: new five.Motor([5, 4])
+    left: new five.Motor([6, 7]),
+    right: new five.Motor([5, 4])
   };
 
   const piezo = new five.Piezo(8);
@@ -36,50 +36,50 @@ board.on('ready', function (err) {
     if ( !key ) { return; }
 
     if ( key.name === 'q' ) {
-      console.log('uitting mBot Controls');
+      console.log('\nQuitting mBot Controls');
       motors.left.stop();
       motors.right.stop();
       process.exit();
     }
 
     if ( key.name === 'space' ) {
-      console.log('Stopping');
+      console.log('\nStopping');
       motors.left.stop();
       motors.right.stop();
     }
 
-    if ( key.name === 'up' ) {
-      console.log('Going forward');
+    if ( key.name === 'up' || key.name === 'w') {
+      console.log('\nGoing forward');
       motors.left.rev(MAX_SPEED);
       motors.right.fwd(MAX_SPEED);
     }
 
-    if ( key.name === 'down' ) {
-      console.log('Going backward');
-      motors.left.fwd(MAX_SPEED);
-      motors.right.rev(MAX_SPEED);
-    }
-
-    if ( key.name === 'f' ) {
-      console.log('lank right');
-      motors.left.rev(MAX_SPEED_TURNS);
-      motors.right.rev(MAX_SPEED_TURNS);
-    }
-
-    if ( key.name === 'a' ) {
-      console.log('bout to be going  left');
+    if ( key.name === 'left' || key.name === 'a') {
+      console.log('\nGoing left');
       motors.left.fwd(MAX_SPEED_TURNS);
       motors.right.fwd(MAX_SPEED_TURNS);
     }
 
+    if ( key.name === 'down' || key.name === 's') {
+      console.log('\nGoing backward');
+      motors.left.fwd(MAX_SPEED);
+      motors.right.rev(MAX_SPEED);
+    }
+
+    if ( key.name === 'right' || key.name === 'd') {
+      console.log('\nGoing right');
+      motors.left.rev(MAX_SPEED_TURNS);
+      motors.right.rev(MAX_SPEED_TURNS);
+    }
+
     if ( key.name === 't' ) {
-      console.log('urbo Boost!!');
+      console.log('\nTurbo Boost!!');
       motors.left.rev(255);
       motors.right.fwd(255);
     }
 
     if ( key.name === 'r' ) {
-      console.log('ick Rolled!!');
+      console.log('\nRick Rolled!!');
       piezo.play(songs.load('never-gonna-give-you-up'));
       motors.left.fwd(255);
       motors.right.fwd(255);
