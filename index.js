aa804ad9bef03d648153f687e02b33db6169d43f
Linda Nichols <lynnaloo@gmail.com>
2016-04-12 22:50:55 -0400

    Server start clean-up

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const server = new Hapi.Server();
 server.connection({ port: env.get('port') });
 
 server.register([
-  require('blipp'),
   require('inert'),
   require('./plugins/controls')
 ], (err) => {
@@ -22,14 +21,13 @@ server.register([
   server.route(routes);
   server.start((err) => {
     if (err) {
+      console.log(err);
       throw err;
     }
 
     console.log('=> Registered plugins:', { plugins: _.keysIn(server.registrations).join(', ') });
-
     console.log('=> Booting server');
-    console.log('=> %s v%s starting on %s', pkg.name, pkg.version, server.info.uri);
+    console.log('=> %s v%s running on %s', pkg.name, pkg.version, server.info.uri);
     console.log('=> Ctrl-C to shutdown server');
-    console.log('=> Server running on %s', server.info.uri);
   });
 });
